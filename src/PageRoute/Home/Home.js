efb325f258e6cf7efb91061f5eb867fdc8f40f0f
Cameron Browne <brownecameron18@gmail.com>
2024-05-18 13:44:58 +0100

    merge

--- src/PageRoute/Home/Home.js
+++ src/PageRoute/Home/Home.js
@@ -1,7 +1,6 @@
 import { Link, NavLink } from "react-router-dom";
 import { React, useState, useEffect } from "react";
 import axios from "axios";
-import "./Inventory.scss";
 
 import { BACKEND_URL } from "../../constants/constants.js";
 
